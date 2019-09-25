beab377b1f4f88b1e6270e19781d7a421316d747
SergioVenero <55211906+SergioVenero@users.noreply.github.com>
2019-09-25 18:07:26 -0500

    s4

--- sesion6a/ejercicio3.cpp
+++ sesion6a/ejercicio3.cpp
@@ -1,48 +1,80 @@
 #include <iostream>
 using namespace std;
 
+void imprimir_notas( int notas[], int n, int p);
+int calcular_promedio(int notas[], int n);
+int calcula_nota_mayor( int notas[], int n);
+int calcula_nota_menor( int notas[], int n);
+void eliminar_nota(int notas[], int n, int nota);
 
-void imprimir_notas(int notas[], int n, int p);
-int calcular_promedio(int notas[],int n);
-int calcular_nota_mayor(int notas[],int n);
-int calcular_nota_menor(int notas[],int n);
-void eliminar_nota(int notas[],int n, int nota);
-int main() {
-
+int main(){
     int n = 0;
-    cout << "Ingrese la cantidad de alumnos: ";
-    cin >> n;
-
+    cout<<"Ingrese la cantidad de alumnos:";
+    cin>>n;
     int *notas = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> notas[i];
+    for(int i=0; i<n; i++){
+        cin>>notas[i];
     }
     imprimir_notas(notas, n, 0);
-    int p=calcular_promedio(notas,n);
+    int p = calcular_promedio(notas, n);
     cout<<"El promedio es: "<<p<<endl;
-    imprimir_notas(notas, n , p);
-    cout<<"La nota mayor es: "<<endl;
-    int menor_nota
+    imprimir_notas(notas, n, p);
+    cout<<"La nota mayor es: "<<calcula_nota_mayor(notas, n)<<endl;
+    int menor_nota = calcula_nota_menor(notas, n);
+    cout<<"La nota menor es: "<<menor_nota<<endl;
+    eliminar_nota(notas, n, menor_nota);
+    cout<<"Nuevo Promedio: "<< calcular_promedio(notas, n)<<endl;
+
     return 0;
 }
 
 void imprimir_notas( int notas[], int n, int p){
-    cout<<"Indice \t valor";
-    for(int i=0;i<n;i++){
-        cout<<i<<"\t"<<notas[i]<<"\n";
+    cout<<"indice | \t valor"<<endl;
+    cout<<"-------|--------------"<<endl;
+    for(int i=0; i<n; i++){
+        if(notas[i] > p){
+            cout<<i<<"\t"<<notas[i]<<endl;
+        }
     }
 }
-int calcular_promedio(int notas[],int n){
-    int suma = 0;
-    for(int i=0;i<n;i++){
-        suma = suma + notas[i];
+
+int calcula_nota_mayor( int notas[], int n){
+    int mayor = 0;
+    for(int i=0; i<n; i++){
+        if(notas[i] > mayor){
+            mayor = notas[i];
+        }
     }
-    return 0;
+    return mayor;
 }
-int calcular_nota_mayor(int notas[],int n){
-    int suma = 0;
-    for(int i=0;i<n;i++){
-        suma = suma + notas[i];
+
+int calcula_nota_menor( int notas[], int n){
+    int menor = 9998998989898;
+    for(int i=0; i<n; i++){
+        if(notas[i] < menor){
+            menor= notas[i];
+        }
     }
-    return 0;
+    return menor;
+}
+
+
+int calcular_promedio(int notas[], int n){
+    int suma = 0;
+    int cant = 0;
+    for(int i=0;i<n; i++){
+        if(notas[i]>=0){
+            suma = suma + notas[i];
+            cant = cant+1;
+        }
+    }
+    return suma/cant;
+}
+
+void eliminar_nota(int notas[], int n, int nota){
+    for(int i=0;i<n; i++){
+        if(notas[i] == nota){
+            notas[i] = -1;
+        }
+    }
 }
